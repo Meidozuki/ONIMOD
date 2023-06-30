40625adf8c90125f08f792fb716158ce63297306
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2023-06-30 19:15:59 +0800

    auto compost 2nd ver

--- compost/Patches.cs
+++ compost/Patches.cs
@@ -1,5 +1,4 @@
 using HarmonyLib;
-using Klei.Actions;
 using UnityEngine;
 
 namespace AutoCompost
@@ -12,15 +11,20 @@ namespace AutoCompost
             [HarmonyPatch(nameof(CompostConfig.CreateBuildingDef))]
             public static void Postfix(ref BuildingDef __result)
             {
+                // Reduce the heat produced
                 __result.ExhaustKilowattsWhenActive = 0f;
-                __result.SelfHeatKilowattsWhenActive = 0.5f;
+                __result.SelfHeatKilowattsWhenActive = 0.1f;
             }
             
             [HarmonyPatch(nameof(CompostConfig.ConfigureBuildingTemplate))]
             public static void Postfix(GameObject go)
             {
+                // Double the capacity
+                go.AddOrGet<ManualDeliveryKG>().capacity = 600f;
+                // Half the work time
+                go.AddOrGet<CompostWorkable>().workTime = 10f;
+                // Text Description
                 go.AddOrGet<BuildingComplete>().isManuallyOperated = false;
-                return;
             }
         }
         
@@ -29,19 +33,20 @@ namespace AutoCompost
         [HarmonyPatch("InitializeStates")]
         public class AutoPatch
         {
-            
             public static void Postfix(Compost.States __instance)
             {
                 ref var self = ref __instance;
                 var actions = self.composting.enterActions;
-                Debug.Log("Actions Length "+ actions.Count.ToString());
+                Debug.Log("FSM Compost.composting Actions Length "+ actions.Count.ToString());
                 actions.RemoveAt(actions.Count - 1);
+
+                self.inert.ScheduleGoTo((smi) => 10.1f, self.composting);
             }
 
         }
 
         [HarmonyPatch(typeof(Compost), MethodType.Constructor)]
-        public class Patch1
+        public class DebugPatch
         {
             public static void Postfix(ref Compost __instance)
             {
