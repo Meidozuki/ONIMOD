d4016f19d1078fbba71b55bdad007a91b903cd13
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2025-12-07 22:13:48 +0800

    Update Assembly date info

--- Mod/SuperMealwood/Properties/AssemblyInfo.cs
+++ Mod/SuperMealwood/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ONI_Mod2")]
+[assembly: AssemblyTitle("SuperMealwood")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.6.29.0")]
-[assembly: AssemblyFileVersion("2021.6.29.0")]
+[assembly: AssemblyVersion("2025.12.7.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
